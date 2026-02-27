53c956eaa46ce788060c9cfe1be9c026902c306c
Garigipati Krishna <sreechaitanya1405@gmail.com>
2026-02-27 12:15:48 +0530

    gf

--- scripts/box_plot.R
+++ scripts/box_plot.R
@@ -5,3 +5,9 @@ View(iris)
 class(iris)
 
 boxplot(iris$Sepal.Length, main = "Box plot for sepal length", ylab = "Sepeal_length", col="skyblue")
+boxplot(iris$Sepal.Length,
+        data = iris,
+        main = "Sepal length by Species",
+        xlab = "Species",
+        ylab = "Sepal Length",
+        col = c("pink","green","skyblue"))
